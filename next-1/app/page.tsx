0ac65cec569ec376fa0bba470fd85e3c20da2abc
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-15 17:22:58 +0300

    Update page.tsx

--- next-1/app/page.tsx
+++ next-1/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello World !!!</h1>
+      <Link href="/about" className="hover:bg-yellow-700 rounded-md">
+        Go to About Page
+      </Link>
     </main>
   );
 }
