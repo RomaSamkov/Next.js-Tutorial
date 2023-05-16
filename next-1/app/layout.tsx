019a926da9f81cd4a7e94a8d2ac2d7302b0807a9
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-16 15:56:22 +0300

    Update layout.tsx

--- next-1/app/layout.tsx
+++ next-1/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav>My NavBar</nav>
+        {/* <nav>My NavBar</nav> */}
         {children}
       </body>
     </html>
